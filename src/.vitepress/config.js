2858fe51d44ad831d6414316a6d5d488579cbc09
GuoJikun <guojikun486546@163.com>
2023-07-10 11:52:29 +0800

    ✨ feat: 添加声明和本地搜索

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -24,5 +24,19 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/vuejs/vitepress" },
     ],
+
+    docFooter: {
+      prev: "上一篇",
+      next: "下一篇",
+    },
+    search: {
+      provider: "local",
+    },
+    outlineTitle: "章节目录",
+    footer: {
+      message:
+        "此文档为非官方翻译版本 - <div class='vp-doc' style='display:inline-flex;'><a href='#' target='__blank'>官方网站</a></div>",
+      copyright: "Copyright © 2023-present GuoJiKun",
+    },
   },
 });
